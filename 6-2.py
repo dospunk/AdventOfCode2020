d67532d385a589755758eff1d59f6fda0c8fe4ec
Connor (Windows) <dospunk@users.noreply>
2020-12-06 23:01:14 -0500

    day 6 working!

--- 6-2.py
+++ 6-2.py
@@ -1,11 +1,10 @@
-#NOT WORKING
-
 import functools as ft
 from pprint import pprint
 
 def getInput(fileLocation: str) -> str:
 	with open(fileLocation, 'r') as file:
-		return file.read()
+		#Thanks to /u/kaspayaz for helping me realize I needed to strip the input string!
+		return file.read().strip()
 
 def getGroupAnswers(input: str) -> list[dict[str,int]]:
 	groupsStrings = input.split("\n\n")
@@ -19,9 +18,9 @@ def getGroupAnswers(input: str) -> list[dict[str,int]]:
 					groupAnswerCounts[char] += 1
 				else:
 					groupAnswerCounts[char] = 1
-		#print(groupString)
-		#pprint(groupAnswerCounts)
-		#print("\n")
+		print(groupString)
+		pprint(groupAnswerCounts)
+		print("\n")
 		out.append(groupAnswerCounts)
 	return out
 
